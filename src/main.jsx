00e9a068d82c6fd1f0781b943d9592e023559650
NickSramcik <Nick.Sramcik@gmail.com>
2023-07-01 13:58:52 -0700

    daisyUI integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom'
 import './index.css'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  document.getElementById('root')
 )
