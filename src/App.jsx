7a6417dfb3dd10d8f9a2cf12b02866d034f90004
funbunch <shannan_bunch@hotmail.com>
2023-07-09 14:42:39 -0700

    Got TW and Daisy to work now. Added initial elements.

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,19 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <div>
-      <h1>Banki Brunch</h1>
-        {/* tailwind */}
-      <button class="btn btn-blue">Button</button>
-      {/* daisy */}
-      <button className="btn btn-primary">Test</button>
-
-      <div className="App">
-      <button className="btn btn-primary">Hello daisyUI</button>
+    <div className="flex flex-col items-center mx-auto font-display">
+      <header className="pt-32 pb-7">
+        <h1 className=" text-white text-[3rem]">Banki Brunch</h1>
+      </header>
+      <main className='flex flex-col items-center max-w-lg text-center font-body'>
+        <button className="btn btn-primary btn-wide text-[1.3rem] mb-12">Get Question</button>
+        <div id="question" className=''>
+          <h2 className='text-[1.5rem]'>What are the pros and cons of using Promises instead of callbacks?</h2>
+        </div>
+        <div id="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet a dignissimos officia nostrum vitae sequi maxime, delectus non iste, error consequatur consequuntur ad deleniti est aspernatur vero laborum tenetur fugiat!Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet a dignissimos officia nostrum vitae sequi maxime, delectus non iste, error consequatur consequuntur ad deleniti est aspernatur vero laborum tenetur fugiat!</p></div>
+      </main>
     </div>
-    </div>
-
-    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      display: ['Lato', 'sans-serif'],
+      body: ['Roboto', 'sans-serif']
+    }
   },
-  plugins: [require('daisyui')],
+  plugins: [require("@tailwindcss/typography"), require('daisyui')],
 }
