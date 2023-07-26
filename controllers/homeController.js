bce7d563dd19c681d9d32451801cdb10e70ecd9c
JHall <JamalHallWebDev@gmail.com>
2023-07-25 20:54:34 -0400

    basic template for backend implementation

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,20 +4,20 @@ class HomeController {
   static home = (req, res) => {
     const message = "Welcome to BANKI BRUNCH!";
     console.log("Home");
-    res.send(message);
+    res.render("home", { message });
   };
 
   static questionsView = (req, res) => {
     console.log("QuestionsView");
     let allQuestions = questions.map((questions) => questions.question);
-    res.send(allQuestions);
+    res.render("questions", { allQuestions });
   };
 
   static typesView = (req, res) => {
     console.log("TypesView");
     let types = questions.map((questions) => questions.type);
     const uniqueTypes = Array.from(new Set(types));
-    res.send(uniqueTypes);
+    res.render("types", { uniqueTypes });
   };
 }
 
--- middleware/logger.js
+++ middleware/logger.js
@@ -7,7 +7,9 @@
  */
 
 function logger(req, res, next) {
-  console.log(`${new Date().toISOString()} - ${req.method} ${req.originalUrl}`);
+  console.log(
+    `${new Date().toLocaleString()} - ${req.method} ${req.originalUrl}`
+  );
   next();
 }
 export default logger;
