a1f12731d622dd417357575f98d19a860fc016e4
lydiadunning <71429289+lydiadunning@users.noreply.github.com>
2023-08-02 13:22:51 -0700

    Update userModel.js
    Remove Question.

--- models/userModel.js
+++ models/userModel.js
@@ -11,18 +11,9 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const questionSchema = new mongoose.Schema([
-  {
-    question: String,
-    type: String,
-    answer: String,
-  },
-]);
 
 const User = mongoose.model("User", userSchema);
-const Question = mongoose.model("Question", questionSchema);
 
 module.exports = {
   User,
-  Question,
 };
