fc5560f238c8b12e08cd47f7edf097a8765278d0
Tim Quattrochi <timq82@gmail.com>
2023-09-26 17:21:26 -0400

    added open to the server object, this automatically opens the browser

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,12 +7,13 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+    open: true, // opens the browser automatically after running npm run dev.
     proxy: {
-      '/auth': {
-        target: 'http://localhost:3000',
+      "/auth": {
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
-      }
-    }
+      },
+    },
   },
-})
+});
