2296bca227d83e015b18cf49d77f29ee00576b80
Tim Quattrochi <timq82@gmail.com>
2023-09-26 22:54:50 -0400

    edited Sun svg component to take fill, className props, added doc

--- src/assets/svg/darkMode/Sun.jsx
+++ src/assets/svg/darkMode/Sun.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-
-const Sun = () => {
+/**
+ * @param {string} fill The fill color of the SVG
+ * @param {string} className The className of the SVG
+ * @returns {JSX.Element} The Sun SVG
+ */
+const Sun = ({ fill, className }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      fill="none"
+      fill={fill}
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="w-6 h-6"
+      fillRule="evenodd"
+      className={className}
     >
       <path
         strokeLinecap="round"
