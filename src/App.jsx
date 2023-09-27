3980faa06f3d000ff7d5b1ffc342d7b18f448806
Tim Quattrochi <timq82@gmail.com>
2023-09-26 20:07:37 -0400

    added in a check for localStorage and set the data-theme attribute based on theme choice

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,81 @@
-import './App.css'
-import AnswerBox from './components/AnswerBox.jsx'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
-import { useAuthContext } from "./contexts/AuthContext"
-import Question from './components/Question.jsx'
-import { useState } from 'react'
-
+import "./App.css";
+import AnswerBox from "./components/AnswerBox.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import { useAuthContext } from "./contexts/AuthContext";
+import Question from "./components/Question.jsx";
+import { useEffect, useState } from "react";
 
 function App() {
-
+  const [theme, setTheme] = useState("dark");
   const auth = useAuthContext();
   const isAuthenticated = auth.isAuthenticated();
 
   function ShowAnswerBtn() {
     const [isOpen, setIsOpen] = useState(false);
     const [visible, setVisible] = useState(true);
-  
+
     function toggle() {
       setIsOpen((isOpen) => !isOpen);
       setVisible((prev) => !prev);
     }
-    
+
+    useEffect(() => {
+      //get access to the html tag so we can change
+      //the data-theme attribute.
+      const html = document.querySelector("html");
+
+      const isThemeInLocalStorage =
+        localStorage.getItem("banki-theme");
+
+      if (isThemeInLocalStorage === "light") {
+        html.setAttribute("data-theme", "light");
+      } else {
+        html.setAttribute("data-theme", "dark");
+      }
+    }, []);
+
     return (
       <div>
         {isOpen && <AnswerBox />}
         {visible && (
-        <button onClick={toggle} className="bg-primary border-[2px] border-full border-accent rounded-full p-2 text-secondary font-bubble tracking-wider text-2xl">GET ANSWER</button>
-            )}
-        </div>
+          <button
+            onClick={toggle}
+            className="bg-primary border-[2px] border-full border-accent rounded-full p-2 text-secondary font-bubble tracking-wider text-2xl"
+          >
+            GET ANSWER
+          </button>
+        )}
+      </div>
     );
   }
 
   return (
-
     <div className="flex flex-col items-center justify-between px-4 mx-auto font-display bg-base-100 h-screen">
-      <Header/>
+      <Header />
 
-      {isAuthenticated ?
+      {isAuthenticated ? (
         <form onSubmit={() => auth.logout()} className="btn-login">
           <button type="submit" className="btn btn-primary">
             Logout
           </button>
         </form>
-        : <form action='/auth/discord' className="btn-login">
+      ) : (
+        <form action="/auth/discord" className="btn-login">
           <button type="submit" className="btn btn-primary">
             Login
           </button>
         </form>
-      }
+      )}
 
-      <main className='flex flex-col gap-4 items-center text-center font-body'>
+      <main className="flex flex-col gap-4 items-center text-center font-body">
         <Question />
         <ShowAnswerBtn />
       </main>
 
-      <Footer />
+      <Footer theme={theme} setTheme={setTheme} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
