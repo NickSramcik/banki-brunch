b48ee6fa9835cdf27915dae9d2900cc6021495d9
Tim Quattrochi <timq82@gmail.com>
2023-09-26 23:07:55 -0400

    added link to github repo, name of site with 100devs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,23 @@ import "../App.css";
 import DarkModeToggle from "./DarkModeToggle";
 function Footer({ theme, setTheme }) {
   return (
-    <div className="bg-neutral justify-between h-10 flex items-center w-screen ">
-      <footer className="bg-neutral">
+    <div className="bg-neutral justify-between h-10 flex items-center w-screen">
+      <span>
         <DarkModeToggle
           dark={true}
           theme={theme}
           setTheme={setTheme}
         />
+      </span>
+      <footer className="bg-neutral p-2">
+        <a
+          href="https://github.com/NickSramcik/banki-brunch"
+          rel="noreferrer"
+          target="_blank"
+          className="text-secondary hover:text-base-100 cursor-pointer"
+        >
+          Banki Brunch &copy; 100devs
+        </a>
       </footer>
     </div>
   );
