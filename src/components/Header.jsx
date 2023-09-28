163073e5c9740f3931d629b73088b5c4ed970598
AhmedHHamdy <ahmedhhamdyad@gmail.com>
2023-09-28 05:31:44 +0300

    mobile responsiveness for the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,10 @@ return(
     <div className="flex items-center justify-between w-screen h-40 pl-4 bg-neutral">
 
       <header className="">
-        <h1 id="logo-text" className=" text-white text-[5rem] ">BANKI BRUNCH</h1>
+        <h1 id="logo-text" className=" text-white lg:text-7xl md:text-6xl text-5xl">BANKI BRUNCH</h1>
       </header>
       
-      <img id="waffle" className="w-80" src="src/assets/waffle.png"/>
+      <img id="waffle" className="lg:w-80 md:w-80 w-72" src="src/assets/waffle.png"/>
     </div>
 )
 }
