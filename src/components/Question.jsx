93f83ad5ffbb11b71f93ac6ab2952659174c50b2
Tim Quattrochi <timq82@gmail.com>
2023-09-27 20:28:15 -0400

    fixed attributes on svg that needed to be camel cased for jsx

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,8 +1,8 @@
-import "../App.css"
-import { useState } from 'react'
-import { questions } from '../../data/questions.js'
-import { parse } from 'node-html-parser'
-import AnswerBox from "./AnswerBox"
+import "../App.css";
+import { useState } from "react";
+import { questions } from "../../data/questions.js";
+import { parse } from "node-html-parser";
+import AnswerBox from "./AnswerBox";
 
 /*
 This shuffle function uses the Fisher-Yates shuffle algorithm
@@ -24,95 +24,111 @@ function shuffle(array) {
 }
 
 export default function Question() {
-
   // Initialize a set of random question indexes per review session
-  const [sessionIndexes, setSessionIndexes] = useState(shuffle(new Array(questions.length).fill(0).map((_, i) => i)));
+  const [sessionIndexes, setSessionIndexes] = useState(
+    shuffle(new Array(questions.length).fill(0).map((_, i) => i))
+  );
 
   // The current index into sessionIndexes
   const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
 
-
   const handleNextQuestion = () => {
     // Increment actionQuestionIndex and rollover if it exceeds
     // the length of the questions Array
-    let next_index = (activeQuestionIndex + 1) % sessionIndexes.length;
-    console.log(activeQuestionIndex, next_index)
-    setActiveQuestionIndex(next_index)
-  }
+    let next_index =
+      (activeQuestionIndex + 1) % sessionIndexes.length;
+    console.log(activeQuestionIndex, next_index);
+    setActiveQuestionIndex(next_index);
+  };
 
   const handlePrevQuestion = () => {
     // To avoid negative indexes, the previous question index is calculated
     // by adding the total number of questions (minus 1) to the current index
-    // and rolls over to the beginning of the range using a modulus whenever 
+    // and rolls over to the beginning of the range using a modulus whenever
     // the new index exceeds the length of the questions Array
-    let prev_index = (activeQuestionIndex + sessionIndexes.length - 1) % sessionIndexes.length;
-    console.log(activeQuestionIndex, prev_index)
-    setActiveQuestionIndex(prev_index)
-  }
-  
+    let prev_index =
+      (activeQuestionIndex + sessionIndexes.length - 1) %
+      sessionIndexes.length;
+    console.log(activeQuestionIndex, prev_index);
+    setActiveQuestionIndex(prev_index);
+  };
+
   function ShowAnswerBtn() {
     const [isOpen, setIsOpen] = useState(false);
     const [visible, setVisible] = useState(true);
     const [buttonText, setButtonText] = useState("GET ANSWER"); //same as creating your state variable where "Next" is the default value for buttonText and setButtonText is the setter function for your state variable instead of setState
-    
-    
+
     function toggle() {
       setIsOpen((isOpen) => !isOpen);
       setVisible((prev) => !prev);
-      setButtonText((state) => (state === "HIDE ANSWER" ? "GET ANSWER" : "HIDE ANSWER"));
+      setButtonText((state) =>
+        state === "HIDE ANSWER" ? "GET ANSWER" : "HIDE ANSWER"
+      );
     }
 
-    
     return (
       <div>
         {isOpen && <AnswerBox />}
-        <button onClick={toggle} className="bg-primary border-[2px] border-full border-accent rounded-full p-4 text-secondary font-bubble tracking-wider text-2xl mt-4">{buttonText}</button>
+        <button
+          onClick={toggle}
+          className="bg-primary border-[2px] border-full border-accent rounded-full p-4 text-secondary font-bubble tracking-wider text-2xl mt-4"
+        >
+          {buttonText}
+        </button>
       </div>
     );
   }
   return (
     <>
-    <div className="flex flex-row justify-center gap-8 mb-8 justify-items-center">
-      <svg
-        onClick={handlePrevQuestion}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="2.5"
-        stroke="currentColor"
-        className="h-20 cursor-pointer w-80" // Set the fixed width and height here
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M15.75 19.5L8.25 12l7.5-7.5"
-        />
-      </svg>
-      <div id="question" className="flex flex-col justify-center w-96">
-        {/* Use the activeQuestionIndex to access the corresponding index into questions for the current session
+      <div className="flex flex-row justify-center gap-8 mb-8 justify-items-center">
+        <svg
+          onClick={handlePrevQuestion}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="2.5"
+          stroke="currentColor"
+          className="h-20 cursor-pointer w-80" // Set the fixed width and height here
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5L8.25 12l7.5-7.5"
+          />
+        </svg>
+        <div
+          id="question"
+          className="flex flex-col justify-center w-96"
+        >
+          {/* Use the activeQuestionIndex to access the corresponding index into questions for the current session
             Then use the parse module to handle any HTML formatting in the question and return the formatted text
         */}
-        <h2 className={`text-[1.5rem] pb-0 text-secondary`}>
-          {parse(questions[sessionIndexes[activeQuestionIndex]].question).text}
-        </h2>
+          <h2 className={`text-[1.5rem] pb-0 text-secondary`}>
+            {
+              parse(
+                questions[sessionIndexes[activeQuestionIndex]]
+                  .question
+              ).text
+            }
+          </h2>
+        </div>
+        <svg
+          onClick={handleNextQuestion}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="2.5"
+          stroke="currentColor"
+          className="h-20 cursor-pointer w-80" // Set the fixed width and height here
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M8.25 4.5l7.5 7.5-7.5 7.5"
+          />
+        </svg>
       </div>
-      <svg
-        onClick={handleNextQuestion}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="2.5"
-        stroke="currentColor"
-        className="h-20 cursor-pointer w-80" // Set the fixed width and height here
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M8.25 4.5l7.5 7.5-7.5 7.5"
-        />
-      </svg>
-    </div>
-    <ShowAnswerBtn/>
+      <ShowAnswerBtn />
     </>
-  )
+  );
 }
